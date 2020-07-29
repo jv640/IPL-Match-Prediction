75cf58899462f0b723669e8088757f8504134262
jv640 <vermajatin640@gmail.com>
2020-07-30 04:11:07 +0530

    done changes player names according to in playing 11

--- checking_playerdata_exist.py
+++ checking_playerdata_exist.py
@@ -5,6 +5,7 @@ playing_11 = pd.read_csv('playing_11.csv')
 rows = playing_11.shape[0]
 counter = 0
 not_found = set()
+players = set()
 for i in range(0, rows):
     player_list = playing_11["Team_1"][i]
     player_list = literal_eval(player_list)
@@ -13,6 +14,7 @@ for i in range(0, rows):
         player = player.strip()
         player = player.replace(" ", "_")
         name =  player + ".csv"
+        players.add(name)
         if os.path.isfile('./Players/' + name):
             print("exist")
         else:
@@ -27,6 +29,7 @@ for i in range(0, rows):
         player = player.strip()
         player = player.replace(" ", "_")
         name =  player + ".csv"
+        players.add(name)
         if os.path.isfile('./Players/' + name):
             print("exist")
         else:
@@ -36,3 +39,10 @@ for i in range(0, rows):
 
 print(not_found)
 print(counter)
+with open('not_found.txt', 'w') as f:
+    for item in not_found:
+        f.write("%s\n" % item)
+with open('all_players.txt', 'w') as f:
+    for item in players:
+        f.write("%s\n" % item)
+
